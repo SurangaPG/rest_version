aad7570f721f5635ca51f69ec4a3f95f5b0bf7cd
Suranga Panagamuwa Gamage <suranga.gamage@one-agency.be>
2017-09-12 10:48:35 +0200

    add standard routing for the details for the versions

--- modules/rest_version_ui/src/Controller/RestVersionUIController.php
+++ modules/rest_version_ui/src/Controller/RestVersionUIController.php
@@ -3,27 +3,41 @@
 namespace Drupal\rest_version_ui\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
 use Drupal\rest_version\Plugin\RestVersionPluginManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Class RestVersionUIController
  *
  * Contains all the route callbacks for the more generic pages.
+ *
+ * @TODO This uses the straight symfony urlGeneratorInterface?
+ * This matches the contrib restui, but seems odd?
  */
 class RestVersionUIController extends ControllerBase {
 
   /**
-   * @var
+   * @var \Drupal\rest_version\Plugin\RestVersionPluginManager
+   *   The plugin manager for all the versions.
    */
   protected $restVersionPluginManager;
+
+  /**
+   * The URL generator to use.
+   *
+   * @var \Symfony\Component\Routing\Generator\UrlGeneratorInterface
+   */
+  protected $urlGenerator;
 
   /**
    * Injects RestUIManager Service.
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('plugin.manager.rest_version.version')
+      $container->get('plugin.manager.rest_version.version'),
+      $container->get('url_generator')
     );
   }
 
@@ -32,9 +46,12 @@ class RestVersionUIController extends ControllerBase {
    *
    * @param \Drupal\rest_version\Plugin\RestVersionPluginManager $restVersionPluginManager
    *   The REST resource plugin manager.
+   * @param \Symfony\Component\Routing\Generator\UrlGeneratorInterface $urlGenerator
+   *   The url generator to use.
    */
-  public function __construct(RestVersionPluginManager $restVersionPluginManager) {
+  public function __construct(RestVersionPluginManager $restVersionPluginManager, UrlGeneratorInterface $urlGenerator) {
     $this->restVersionPluginManager = $restVersionPluginManager;
+    $this->urlGenerator = $urlGenerator;
   }
 
   /**
@@ -44,13 +61,38 @@ class RestVersionUIController extends ControllerBase {
 
     $build = [];
 
-    
+    // @TODO Move this to a proper listbuilding class.
+    $build['version_list'] = [
+      '#type' => 'table',
+      '#header' => [t('Label'), t('Machine name'), t('Operations')]
+    ];
 
     foreach ($this->restVersionPluginManager->getDefinitions() as $definition) {
-
+      $build['version_list']['#rows'][]['data'] = [
+        $definition['label'],
+        $definition['machineName'],
+        'operations' => [
+          'data' => [
+            '#type' => 'operations',
+            '#links' => [
+              'enable' => [
+                'title' => $this->t('View'),
+                'url' => Url::fromRoute('rest_version_ui.version.canonical', ['version_id' => $definition['machineName']]),
+              ],
+            ],
+          ]
+        ]
+      ];
     }
 
     return $build;
+  }
+
+  /**
+   * Displays a single version for of the rest api.
+   */
+  public function viewVersion() {
+    return [];
   }
 
 }
